4ee35ac00f5cd411ebf8ab3acc685c3f4fdf16de
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-03-15 10:13:32 +0200

    Add configs to requests

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,49 +1,50 @@
+import { AxiosRequestConfig } from 'axios';
 import instance from './axiosInterceptors';
 
 export const client = {
-  async get<T>(url: string) {
-    const response = await instance.get<T>(url);
+  async get<T>(url: string, config?: AxiosRequestConfig) {
+    const response = await instance.get<T>(url, config);
 
     return response.data;
   },
 
-  async post<T>(url: string, data?: unknown) {
+  async post<T>(url: string, data?: unknown, config?: AxiosRequestConfig) {
     if (typeof data === 'undefined') {
       const response = await instance.post<T>(url);
 
       return response.data;
     }
 
-    const response = await instance.post<T>(url, data);
+    const response = await instance.post<T>(url, data, config);
 
     return response.data;
   },
 
-  async patch<T>(url: string, data?: unknown) {
+  async patch<T>(url: string, data?: unknown, config?: AxiosRequestConfig) {
     if (typeof data === 'undefined') {
       const response = await instance.post<T>(url);
 
       return response.data;
     }
 
-    const response = await instance.post<T>(url, data);
+    const response = await instance.post<T>(url, data, config);
 
     return response.data;
   },
 
-  async put<T>(url: string, data?: unknown) {
+  async put<T>(url: string, data?: unknown, config?: AxiosRequestConfig) {
     if (typeof data === 'undefined') {
       const response = await instance.put<T>(url);
 
       return response.data;
     }
 
-    const response = await instance.put<T>(url, data);
+    const response = await instance.put<T>(url, data, config);
 
     return response.data;
   },
 
-  async delete(url: string) {
-    return instance.delete(url);
+  async delete(url: string, config?: AxiosRequestConfig) {
+    return instance.delete(url, config);
   },
 };
