a41e116168b5a62a876932b1e7ed9bb7ee9e87a4
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-03-19 21:29:57 +0200

    Add createMaster api

--- src/api/master.ts
+++ src/api/master.ts
@@ -1,7 +1,11 @@
 import { Page } from '../types/main';
-import { MasterCard } from '../types/master';
+import { CreateMaster, MasterCard } from '../types/master';
 import { client } from '../utils/axiosClient';
 
 export const getMasterCard = (pageNumber: number, pageSize: number) => {
   return client.get<Page<MasterCard>>(`/masterSortByRating?pageNumber=${pageNumber}&pageSize=${pageSize}`);
+};
+
+export const createMaster = (master: CreateMaster) => {
+  return client.post('/new-master', master);
 };
